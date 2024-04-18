9d77bb5df733db673ed89810e270d96ec89f7dc8
Nedgs <nadergwasmi25@gmail.com>
2024-04-18 09:54:56 +0200

    fix timeout

--- automate.js
+++ automate.js
@@ -3,86 +3,80 @@ require('dotenv').config();
 
 (async () => {
     const browser = await chromium.launch({
-        headless: false, // Ouvrir le navigateur en mode non headless
+        headless: false,
+        args: ["--start-maximized"]
     });
 
     const context = await browser.newContext({
-        viewport: { width: 1920, height: 1080 } // Définir la taille du viewport pour correspondre à la résolution de votre écran
+        viewport: null
     });
 
-    // Créer une nouvelle page dans le contexte
     const page = await context.newPage();
-
-
 
     try {
         let responseBody = {};
-        // Intercepter les réponses avant toute autre chose
+        // Intercept responses before anything else
         page.on('response', async (response) => {
             const url = response.url();
             const regex = /\/activity\/\d+\/content\/\d+/;
             if (regex.test(url)) {
-                console.log('Requête interceptée:', url);
+                console.log('Intercepted request:', url);
                 responseBody = await response.json();
-                console.log('Contenu de la réponse:', responseBody.props.examQuestions.data);
-                // Effectuer d'autres opérations avec le contenu de la réponse si nécessaire
+                console.log('Response content:', responseBody.props.examQuestions.data);
             }
         });
 
-        // Naviguer vers la page de connexion
+        // Navigate to login page
         await page.goto('https://auth.global-exam.com/login');
 
-        // Remplir les champs de connexion
+        // Fill in login fields
         await page.fill('input[name="email"]', process.env.EMAIL);
         await page.fill('input[name="password"]', process.env.PASSWORD);
         await page.click('button[class="button-solid-primary-big mb-6"]');
 
-        // Attendre un délai aléatoire court
+        // Wait for a short random delay
         await page.waitForTimeout(randomDelay(1000, 3000));
 
-        // Faire défiler la page de 500 pixels
+        // Scroll the page by 500 pixels
         await page.evaluate(() => {
             window.scrollBy(0, 500);
         });
 
-        // Cliquer sur le premier lien <a> dans la div avec la classe 'grid-cols-1'
+        // Click the first <a> link inside div with class 'grid-cols-1'
         await page.click('div.grid-cols-1 a');
 
-        // Attendre un délai aléatoire court
-        await page.waitForTimeout(randomDelay(5000, 6000));
-
-        // Cliquer sur le bouton de consentement (supposons qu'il ait l'ID 'axeptio_btn_acceptAll')
-        await page.click('#axeptio_btn_acceptAll');
-
-        await page.selectOption('select', 'A2'); // Sélectionner une option dans un élément <select>
-
-        // Attendre un délai aléatoire court
         await page.waitForTimeout(randomDelay(1000, 2000));
 
-        // Récupérer et traiter les éléments div parent et enfant désirés
+        // Click the consent button (assuming it has ID 'axeptio_btn_acceptAll')
+        await page.click('#axeptio_btn_acceptAll');
+
+        await page.selectOption('select', 'A2'); // Select an option in a <select> element
+
+        await page.waitForTimeout(randomDelay(1000, 2000));
+
+        // Retrieve and process desired parent and child div elements
         const bigParentDiv = await page.$('.container.lg\\:mt-6');
         const parentDiv = await bigParentDiv.$('div');
         const childDivs = await parentDiv.$$('div');
 
-        // Parcourir chaque div dans childDivs
         for (const div of childDivs) {
             const buttons = await div.$$('button');
 
             for (const button of buttons) {
-                await button.click(); // Cliquer sur chaque bouton à l'intérieur de la div
-                console.log('Cliqué sur un bouton à l\'intérieur de la div');
-                await page.waitForTimeout(randomDelay(3000, 5000)); // Attendre un court délai après chaque clic
-
+                await button.click();
+                console.log('Clicked a button inside the div');
+                await page.waitForTimeout(randomDelay(3000, 5000));
                 responseBody.props.examQuestions.data.sort((a, b) => a.order - b.order);
 
-                // Récupérer tous les champs d'entrée dans le DOM
+                // Retrieve all input fields in the DOM
                 const inputFields = await page.$$('input[type="text"]');
 
-                // Remplir chaque champ d'entrée avec une valeur différente
+                // Fill each input field with a different value
                 for (let i = 0; i < inputFields.length; i++) {
                     const question = responseBody.props.examQuestions.data[i];
                     const input = inputFields[i];
                     await input.fill(question.exam_answers[0].name);
+                    await page.waitForTimeout(randomDelay(1000, 1200));
                 }
             }
         }
@@ -91,30 +85,23 @@ require('dotenv').config();
 
         await page.waitForTimeout(randomDelay(2000, 3000));
 
-        // Cliquer sur le bouton de soumission du formulaire qui a la classe 'button-solid-primary-large'
+        // Click the form submission button with class 'button-solid-primary-large'
         await page.click('.button-solid-primary-large');
 
         console.log('here 2');
 
-
         await page.waitForTimeout(randomDelay(3000, 4000));
 
         console.log('here 3');
-
-        // // Attendre un délai aléatoire court
-        // await page.waitForTimeout(randomDelay(1000, 2000));
-
-        // // Cliquer sur le bouton
-        // await page.click('.button-solid-primary-large');
     } catch (error) {
-        console.error('Une erreur s\'est produite:', error);
+        console.error('An error occurred:', error);
     } finally {
-        // Fermer le navigateur une fois toutes les opérations terminées
+        // Close the browser once all operations are completed
         await browser.close();
     }
 })();
 
-// Fonction pour générer un délai aléatoire entre minDelay et maxDelay (en millisecondes)
+// Function to generate a random delay between minDelay and maxDelay (in milliseconds)
 function randomDelay(minDelay, maxDelay) {
     return Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
 }
